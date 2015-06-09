2e34514628240b0c2b20e984336a155ccdf56077
Devorah <devorah@Mandelbaum.local>
2015-06-09 00:26:34 -0400

    setting rough structure and modularizing

--- static/script/script.js
+++ static/script/script.js
@@ -1,50 +1,121 @@
 $(function(){
 	'use strict';
 
-	var data = {
-		'application_type': ['corporate','ira','trust','partnership','llc','individual','joint'],
-		'ccy': ['aud','cad','chf','eur','gbp','hkd','jpy','nzd','usd'],
-		'country': ['afghanistan','albania','algeria','american_samoa','andorra','angola','anguilla','antigua_and_barbuda','argentina','armenia','aruba','austria','australia','azerbaijan'],
-		'rb': ['fxcm', 'mt4'],
-		'execution': ['dealing_desk', 'no_dealing_desk'],
-		'locale': ['ar_AE', 'de_DE'],
-		'platform': ['mt4', 'trading_station', 'active_trader'],
-		'product': ['spread_bet', 'fx'],
-		'service_level': ['1', '2', '3', '4']
+	var util = {
+		appendHTML: function(child, parent){
+
+		},
+		crossBrowserEventObj: function(){
+
+		}
 	};
 
-	function sourceCallback( term, suggest, dataCategory ){
-		term = term.toLowerCase();
-		var choices = data[dataCategory];
-		var suggestions = [];
-		for ( i = 0; i < choices.length; i++ ) {
-			if ( choices[i].toLowerCase().indexOf(term) > -1 ) {
-				suggestions.push(choices[i]);
+	var data = {
+		query_params : {
+			'application_type': ['corporate','ira','trust','partnership','llc','individual','joint'],
+			'ccy': ['aud','cad','chf','eur','gbp','hkd','jpy','nzd','usd'],
+			'country': ['afghanistan','albania','algeria','american_samoa'],
+			'rb': ['fxcm', 'mt4'],
+			'execution': ['dealing_desk', 'no_dealing_desk'],
+			'locale': ['ar_AE', 'de_DE'],
+			'platform': ['mt4', 'trading_station', 'active_trader'],
+			'product': ['spread_bet', 'fx'],
+			'service_level': ['1', '2', '3', '4']
+		},
+		url_settings : {
+			'Environment': {
+				'qa': ["http://qa"],
+				'uat': ["http://uat"],
+				'prod': ["http://prod"]
+			},
+			'Protocol': {
+				'https': ['https'],
+				'http': ['http']
 			}
 		}
-		suggest(suggestions);
-	}
+	};
 
-	function renderItemCallback( item, search ){
-		return '<div class="autocomplete-suggestion">' + item + '</div>';
-	}
+	var url = {
+		$el: $('#generated_url'),
+		render: function(){
+			console.log('rendering the url');
+			url.$el.text('this is test content');
+		},
+		init: function(){
+			url.render();
+		}
+	};
 
-	function onSelectCallback( e, term, item){
-		console.log('Item "' + item.text() + '" selected by ' + (e.type == 'keydown' ? 'pressing enter' : 'mouse click') + '.');
-	}
+	var url_settings = {
+		$el: $('#url_settings').find('.button'),
 
-	// Automcomplete
-	// http://goodies.pixabay.com/jquery/auto-complete/demo.html
-	// Using the renderItem function to turn the autocomplete into a combo autocomplete with select menu functionality
-	$('[data-autocomplete]').each(function(){
-		var dataCategory = $(this).attr('data-category');
+		select: function(){
+			console.log('selecting');
+		},
+		bind: function(){
+			console.log('binding');
+			url_settings.$el.each(function(){
+				$(this).on('click', function(){
+					url_settings.select();
+				} );
+			});
 
-		$(this).autoComplete({
-			minChars: 0,
-			source: function( term, suggest ){
-				sourceCallback( term, suggest, dataCategory );
+		},
+		init: function(){
+			url_settings.bind();
+		}
+
+	};
+
+	var autocomplete = {
+		$el: $('[data-autocomplete]'),
+		template: $(),
+
+		sourceCallback: function( term, suggest, category ){
+			term = term.toLowerCase();
+			var choices = data.query_params[category] || [];
+			var suggestions = [];
+			for ( var i = 0; i < choices.length; i++ ) {
+				if ( choices[i].toLowerCase().indexOf(term) > -1 ) {
+					suggestions.push(choices[i]);
+				}
 			}
-		});
-	});
-	
+			suggest(suggestions);
+		},
+
+		renderItemCallback: function (item, search, category){
+			var category = category || "not defined";
+			var re = new RegExp("(" + search.split(' ').join('|') + ")", "gi");
+			return '<div class="autocomplete-suggestion" data-val="' + item + '">' + item.replace(re, "<b>$1</b>") + '</div>';
+		},
+
+		onSelectCallback: function( e, term, item){
+			console.log('updating ' + item.attr('data-category') + '. New value is: ' + item.text() );
+			url.render();
+		},
+
+		init: function(){
+			// Automcomplete
+			// http://goodies.pixabay.com/jquery/auto-complete/demo.html
+			// Using the renderItem function to turn the autocomplete into a combo autocomplete with select menu functionality
+			autocomplete.$el.each(function(){
+				var category = $(this).attr('data-category');
+				$(this).autoComplete({
+					minChars: 0,
+					delay:75,
+					source: function( term, suggest ){
+						autocomplete.sourceCallback( term, suggest, category);
+					},
+					renderItem: autocomplete.renderItemCallback,
+					onSelect: autocomplete.onSelectCallback
+				});
+			});
+		}
+	};
+
+	// Setup
+	autocomplete.init();
+	url.init();
+	url_settings.init();
+
 });
