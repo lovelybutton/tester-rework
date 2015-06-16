070d1a0473e74afa451cfd50db6cb1421be2a00a
Devorah <devorah@Mandelbaum.local>
2015-06-15 23:20:06 -0400

    fix autosuggest limit issue

--- static/script/script.js
+++ static/script/script.js
@@ -217,7 +217,6 @@ $(function(){
 			opts: {
 				highlight: true,
 				hint: false,
-				limit:10,
 				minLength:0
 			}
 		},
@@ -229,21 +228,19 @@ $(function(){
 
 			// attach typeahead functionality
 			item.typeahead(autocomplete.defaults.opts, {
+				name: item.data('category'),
 				source: autocomplete.defaults.filterFn(dataset),
+				limit:300 // docs say to put limit in the first config object, but it only worked when I placed it here
 			});
 
-			// Attach change handler
-			// Must be attached directly to each instance via native change event
-			// At current time, typeahead's change event is not working
-			// Therefore we cannot use event delegation.
-			item.on('change', handlers.onChange);
-
-			$(document).on('typeahead:active', handlers.onActive);
-			$(document).on('typeahead:close', handlers.onClose);
-			$(document).on('typeahead:open', handlers.onOpen);
-			$(document).on('typeahead:render', handlers.onRender);
-			$(document).on('typeahead:select', handlers.onSelect);
-			$(document).on('typeahead:idle', handlers.onIdle);
+			// Attach handlers
+			item.on('change', handlers.onChange); // Binding to native change event because at time of writing, typeahead's change event is not working
+			item.on('typeahead:active', handlers.onActive);
+			item.on('typeahead:close', handlers.onClose);
+			item.on('typeahead:open', handlers.onOpen);
+			item.on('typeahead:render', handlers.onRender);
+			item.on('typeahead:select', handlers.onSelect);
+			item.on('typeahead:idle', handlers.onIdle);
 		}
 	};
 
@@ -498,7 +495,7 @@ $(function(){
 					var el = $(e.target);
 					el.addClass('active').closest('ul').addClass('autocompleteOpen')
 				},
-				onRender: function(){},
+				onRender: function(e){},
 				onSelect: function(e){
 					params.onUpdate(e.target);
 				}
